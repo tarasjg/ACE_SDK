2a44f61bf71fd72a5e4c01f44f58ce313a7a7023
John Tarasidis <tarasidisj@gmail.com>
2021-02-18 20:18:24 -0500

    presenter.py tentatively done

--- External Applications/presenter.py
+++ External Applications/presenter.py
@@ -1,11 +1,31 @@
-import serial
-import csv
+import serial  # to establish com port
+import csv     # to r/w .csv
+import sys     # to parse argv
 
 
-#either grab port from arg or grep PID/VID
+# on linux, we should run lsusb and find the port with the correct PID/VID
+# and then run this applet in terminal >python presenter.py -p /dev/ttyx
 
-#send dump token
+# format ['presenter.py', '-p', port]
+if sys.argv[1] == '-p':
+    port = sys.argv[2]
+else:
+    print('Invalid arguments: -p [port]')
+    sys.exit()
 
-#receive everything in chunks
+# now that we have the port, lets open it
+ser = serial.Serial(port)
+ser.write(0xAA)  # magic number that triggers a dump in firmware
 
-#write to csv
+data = ser.read(8e+6)  # read 8 megabytes of data, we should not do this
+
+# I think more likely we will be getting things in 1kB chunks
+# but need to figure out how we will do it in firmware to fix this part
+
+# now convert bytes to ints and write row-wise to an output csv
+
+data = int.from_bytes(data, byteorder='big', signed=True)
+
+with open('dump.csv', 'w', newline='') as file:
+    writer = csv.writer(file)
+    writer.writerows(data)
