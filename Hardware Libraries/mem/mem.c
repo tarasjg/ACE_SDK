7c5ca0046cb4b9c16aa27efeeb2f99b10a0d3cbc
John Tarasidis <tarasidisj@gmail.com>
2021-03-30 00:18:23 -0400

    added mem_init, tests pass

--- Hardware Libraries/mem/mem.c
+++ Hardware Libraries/mem/mem.c
@@ -7,19 +7,94 @@
 #include "main.h"
 #include "mem.h"
 
+extern QSPI_HandleTypeDef hqspi;
+
 /*
  * This function performs the following set up flow:
  *
  * Write Enable - single cmd
- * Check Write Enable Latch - autopoll; if timeout return fail init
+ * Check Write Enable Latch (bit1) - autopoll; if timeout return fail init
  * Write Status Register Quad Enable - single cmd
- * Check Write In-Progress -autopoll; if timeout return fail init
- * Read Status Register Quad Enable - single cmd
+ * Check Write In-Progress (bit0) -autopoll; if timeout return fail init
+ * Read Status Register Quad Enable (bit6) - single cmd
  *
  * if QE Bit high, return 1 and exit
  */
 uint8_t mem_init(void) {
+	//WREN - p20 on datasheet
+	QSPI_CommandTypeDef write_enable;
+	write_enable.InstructionMode = QSPI_INSTRUCTION_1_LINE;
+	write_enable.Instruction = 0x06;
+	write_enable.AddressMode = QSPI_ADDRESS_NONE;
+	write_enable.AlternateByteMode = QSPI_ALTERNATE_BYTES_NONE;
+	write_enable.DataMode = QSPI_DATA_NONE;
+	write_enable.NbData = 0;
+	write_enable.DummyCycles = 0;
+	write_enable.DdrMode = QSPI_DDR_MODE_DISABLE;
+	write_enable.SIOOMode = QSPI_SIOO_INST_EVERY_CMD;
 
+	HAL_QSPI_Command(&hqspi, &write_enable, HAL_MAX_DELAY);
+
+	QSPI_CommandTypeDef read_status_register;
+	read_status_register.InstructionMode = QSPI_INSTRUCTION_1_LINE;
+	read_status_register.Instruction = 0x05;
+	read_status_register.AddressMode = QSPI_ADDRESS_NONE;
+	read_status_register.AlternateByteMode = QSPI_ALTERNATE_BYTES_NONE;
+	read_status_register.DataMode = QSPI_DATA_1_LINE;
+	read_status_register.NbData = 1;
+	read_status_register.DummyCycles = 0;
+	read_status_register.DdrMode = QSPI_DDR_MODE_DISABLE;
+	read_status_register.SIOOMode = QSPI_SIOO_INST_EVERY_CMD;
+
+	QSPI_AutoPollingTypeDef RDSR_WEL;
+	RDSR_WEL.Match = 0x02;
+	RDSR_WEL.Mask = 0x02;
+	RDSR_WEL.Interval = 5;
+	RDSR_WEL.StatusBytesSize = 0x01;
+	RDSR_WEL.MatchMode = QSPI_MATCH_MODE_AND;
+	RDSR_WEL.AutomaticStop = QSPI_AUTOMATIC_STOP_ENABLE;
+
+	if (HAL_QSPI_AutoPolling(&hqspi, &read_status_register, &RDSR_WEL, 15) == HAL_ERROR) {
+		return MEM_INIT_FAIL;
+	}
+
+	QSPI_CommandTypeDef write_status_register;
+	write_status_register.InstructionMode = QSPI_INSTRUCTION_1_LINE;
+	write_status_register.Instruction = 0x01;
+	write_status_register.AddressMode = QSPI_ADDRESS_NONE;
+	write_status_register.AlternateByteMode = QSPI_ALTERNATE_BYTES_NONE;
+	write_status_register.DataMode = QSPI_DATA_1_LINE;
+	write_status_register.NbData = 3;
+	write_status_register.DummyCycles = 0;
+	write_status_register.DdrMode = QSPI_DDR_MODE_DISABLE;
+	write_status_register.SIOOMode = QSPI_SIOO_INST_EVERY_CMD;
+
+	uint8_t QEN[3] = {0x40, 0x00, 0x00};
+	HAL_QSPI_Command(&hqspi, &write_status_register, HAL_MAX_DELAY);
+	HAL_QSPI_Transmit(&hqspi, QEN, HAL_MAX_DELAY);
+	HAL_Delay(1);
+
+	QSPI_AutoPollingTypeDef RDSR_WIP;
+	RDSR_WIP.Match = 0x00;
+	RDSR_WIP.Mask = 0x01;
+	RDSR_WIP.Interval = 5;
+	RDSR_WIP.StatusBytesSize = 0x01;
+	RDSR_WIP.MatchMode = QSPI_MATCH_MODE_AND;
+	RDSR_WIP.AutomaticStop = QSPI_AUTOMATIC_STOP_ENABLE;
+
+	if (HAL_QSPI_AutoPolling(&hqspi, &read_status_register, &RDSR_WIP, 15) == HAL_ERROR) {
+		return MEM_INIT_FAIL;
+	}
+
+	HAL_QSPI_Command(&hqspi, &read_status_register, HAL_MAX_DELAY);
+	uint8_t stat = 0;
+	HAL_QSPI_Receive(&hqspi, &stat, HAL_MAX_DELAY);
+
+	if (stat & QEN[0]) {
+		return MEM_INIT_SUCCESS;
+	} else {
+		return MEM_INIT_FAIL;
+	}
 }
 
 /*
--- Hardware Libraries/mem/mem.h
+++ Hardware Libraries/mem/mem.h
@@ -8,6 +8,15 @@
 #ifndef INC_MEM_H_
 #define INC_MEM_H_
 
+#define MEM_INIT_SUCCESS 0x01
+#define MEM_INIT_FAIL	 0x00
+
+#define MEM_ERASE_SUCCESS 0x01
+#define MEM_ERASE_TIMEOUT 0x00
+
+#define MEM_WRITE_SUCCESS 0x01
+#define MEM_WRITE_TIMEOUT 0x00
+
 uint8_t mem_init(void);
 
 void mem_read(uint32_t, uint8_t*);
